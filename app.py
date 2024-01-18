e48f946f17c525459b160d0e9c3d038745bc9dee
Rodger Cabrerisso <83619204+rodgerhades@users.noreply.github.com>
2024-01-18 20:12:01 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+nome = "Rodger " string
+idade = 31 int
+altura = 1.81 float
+is_male = True/false bool
+gosto_de = "programação"
+
+print(f" meu nome é {nome} ")
+print(f" eu gosto de {gosto_de} ")
+print(f" eu tenho {idade} anos ")
+print(f" {nome} gosta de {gosto_de} e tem {idade} anos ")
